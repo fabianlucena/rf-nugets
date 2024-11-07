6e985e744879f2fd289407471ddb87f95713435a
Fabian Lucena <fabianlucena@gmail.com>
2024-11-07 13:12:23 -0300

    Add RFUserEmailVerified.

---
+++ RFUserEmailVerified/IServices/IUserEmailService.cs
@@ -0,0 +1,13 @@
+﻿using RFService.IService;
+using RFService.Repo;
+using RFUserEmail.Entities;
+
+namespace RFUserEmail.IServices
+{
+    public interface IUserEmailService : IServiceId<UserEmail>
+    {
+        Task<UserEmail?> GetSingleOrDefaultForUserIdAsync(Int64 userId, GetOptions? options = null);
+
+        Task SetIsVerifiedForIdAsync(bool isVerified, Int64 id);
+    }
+}
---
+++ RFUserEmailVerifiedDapper/Setup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RFDapper;
+using RFUserEmail.Entities;
+
+namespace RFUserEmailDapper
+{
+    public static class Setup
+    {
+        public static void ConfigureRFUserEmailDapper(IServiceProvider services)
+        {
+            var dapperService = services.GetService<Dapper<UserEmail>>() ??
+                throw new Exception($"No service UserEmail");
+
+            dapperService.CreateTable();
+        }
+    }
+}
