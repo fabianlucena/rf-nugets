e2bc27fc7c8a20fcd39135c3cee96085e5cd3aa7
Fabian Lucena <fabianlucena@gmail.com>
2024-11-24 18:31:06 -0300

    Add GetListIdAsync method.

--- RFService/IServices/IServiceId.cs
+++ RFService/IServices/IServiceId.cs
@@ -8,6 +8,9 @@ namespace RFService.IServices
     {
         Int64 GetId(Entity item);
 
+        public async Task<IEnumerable<Int64>> GetListIdAsync(GetOptions options)
+            => (await GetListAsync(options)).Select(GetId);
+
         Task<Entity> GetForIdAsync(Int64 id, GetOptions? options = null);
 
         Task<Entity?> GetSingleOrDefaultForIdAsync(Int64 id, GetOptions? options = null);
