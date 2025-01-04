4c96920eca7785d6b78b470f4c410cd84e519f21
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2025-01-04 19:17:32 -0300

    RF Service add Column, IsNull, EQ, and And operators.

--- RFService/Operator/Operators.cs
+++ RFService/Operator/Operators.cs
@@ -1,5 +1,10 @@
 ﻿namespace RFService.Operator
 {
+    public class Column(string value)
+    {
+        public string Value { get; } = value;
+    }
+
     public class DistinctTo(object value)
     {
         public object Value { get; } = value;
@@ -9,6 +14,22 @@
     {
     }
 
+    public class IsNull(object value)
+    {
+        public object Value { get; } = value;
+    }
+
+    public class Eq(object value1, object value2)
+    {
+        public object Value1 { get; } = value1;
+        public object Value2 { get; } = value2;
+    }
+
+    public class And(params object[] values)
+    {
+        public object[] Values { get; } = values;
+    }
+
     public class GE(object value)
     {
         public object Value { get; } = value;
@@ -16,8 +37,12 @@
 
     public class Op
     {
+        public static Column Column(string value) => new(value);
         public static DistinctTo DistinctTo(object value) => new(value);
         public static NotNull NotNull() => new();
+        public static IsNull IsNull(object value) => new(value);
+        public static Eq Eq(object value1, object value2) => new(value1, value2);
+        public static And And(params object[] values) => new(values);
         public static GE GE(object value) => new(value);
     }
 }
