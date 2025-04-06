2fd034a3edb35bcd6d447fd0af0d86a1fe8bed5e
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2025-04-06 01:25:00 -0300

    Add RFDBLocalizerDapper package.

---
+++ RFDBLocalizerDapper/Services.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RFDapper;
+using RFDBLocalizer.Entities;
+using RFLocalizer.Entities;
+using RFService.IRepo;
+
+namespace RFDBLocalizerDapper
+{
+    public static class MvcServiceCollectionExtensions
+    {
+        public static void AddRFDBLocalizerDapper(this IServiceCollection services)
+        {
+            services.AddScoped<Dapper<Language>, Dapper<Language>>();
+            services.AddScoped<Dapper<Context>, Dapper<Context>>();
+            services.AddScoped<Dapper<Source>, Dapper<Source>>();
+            services.AddScoped<Dapper<Translation>, Dapper<Translation>>();
+
+            services.AddScoped<IRepo<Language>, Dapper<Language>>();
+            services.AddScoped<IRepo<Context>, Dapper<Context>>();
+            services.AddScoped<IRepo<Source>, Dapper<Source>>();
+            services.AddScoped<IRepo<Translation>, Dapper<Translation>>();
+        }
+    }
+}
---
+++ RFDBLocalizerDapper/Setup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RFDapper;
+using RFDBLocalizer.Entities;
+using RFLocalizer.Entities;
+
+namespace RFDBLocalizerDapper
+{
+    public static class Setup
+    {
+        public static void ConfigureRFDBLocalizerDapper(IServiceProvider services)
+        {
+            CreateTable<Language>(services);
+            CreateTable<Context>(services);
+            CreateTable<Source>(services);
+            CreateTable<Translation>(services);
+        }
+
+        public static void CreateTable<TEntity>(IServiceProvider services)
+            where TEntity : class
+        {
+            var dapperService = services.GetService<Dapper<TEntity>>() ??
+                throw new Exception($"No service {typeof(TEntity).Name}");
+
+            dapperService.CreateTable();
+        }
+    }
+}
