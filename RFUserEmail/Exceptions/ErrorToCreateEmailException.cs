eb2e7aaf7ca7bfa41dd7bfb55b4a021287b2729e
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2025-04-21 18:37:26 -0300

    Fix translations.

--- RFUserEmail/Exceptions/ErrorToCreateEmailException.cs
+++ RFUserEmail/Exceptions/ErrorToCreateEmailException.cs
@@ -3,7 +3,7 @@
 namespace RFUserEmail.Exceptions
 {
     public class ErrorToCreateEmailException()
-        : HttpException(404, "Error to create email")
+        : HttpException(404, "Error creating email")
     {
     }
 }
--- RFUserEmailVerified/Exceptions/ErrorToCreateEmailException.cs
+++ RFUserEmailVerified/Exceptions/ErrorToCreateEmailException.cs
@@ -3,7 +3,7 @@
 namespace RFUserEmailVerified.Exceptions
 {
     public class ErrorToCreateEmailException()
-        : HttpException(404, "Error to create email")
+        : HttpException(404, "Error creating email")
     {
     }
 }
