afd961682e5eab947557c64a2efc20997db22393
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2025-04-21 18:25:16 -0300

    RHAuth ES: fix tranalation file name.

--- RFAuth_es/Setup.cs
+++ RFAuth_es/Setup.cs
@@ -8,7 +8,7 @@ namespace RFAuth_es
         public static void ConfigureDataRFAuthEs(IServiceProvider provider)
         {
             var l10n = provider.GetRequiredService<IL10n>();
-            l10n.AddTranslationsFromFile("es", "", "translations.txt");
+            l10n.AddTranslationsFromFile("es", "", Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "rfauth_es.txt"));
         }
     }
 }
