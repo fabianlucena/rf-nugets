5e173d4e44b4c87f661912d8472cad6c5bcd2a75
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2025-05-20 15:48:19 -0300

    Add params to HTTPException.

--- RFHttpExceptions/Exceptions/HttpException.cs
+++ RFHttpExceptions/Exceptions/HttpException.cs
@@ -2,9 +2,23 @@
 
 namespace RFHttpExceptions.Exceptions
 {
-    public class HttpException(int statusCode, string? message = null)
-        : Exception(message), IHttpException
+    public class HttpException(int statusCode, string? FormatMessage = null, params string[] ParamsList)
+        : Exception(), IHttpException
     {
         public int StatusCode { get => statusCode; }
+        override public string? Message { get => GetMessage(); }
+
+        public string? GetMessage()
+        {
+            if (FormatMessage == null || ParamsList.Length == 0)
+                return FormatMessage;
+            else
+            {
+                var message = FormatMessage;
+                for (int i = 0; i < ParamsList.Length; i++)
+                    message = message.Replace($"{{{i}}}", ParamsList[i]);
+                return message;
+            }
+        }
     }
 }
