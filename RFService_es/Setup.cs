cfdc4d921e4733b4a3317c8969da8b20da261337
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2025-07-31 11:41:36 -0300

    RFService_es: add project.

---
+++ RFService_es/Setup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RFL10n;
+
+namespace RFService_es
+{
+    public static class Setup
+    {
+        public static void ConfigureDataRFServiceEs(IServiceProvider provider)
+        {
+            var l10n = provider.GetRequiredService<IL10n>();
+            l10n.AddTranslationsFromFile("es", "", Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "rfservice_es.txt"));
+        }
+    }
+}
