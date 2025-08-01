942cd160886673a9682c36cfd1bb608da2f083dc
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2025-08-01 09:30:40 -0300

    RFService: fix syntax error.

--- RFService/Exceptions/IdItemNotFoundException.cs
+++ RFService/Exceptions/IdItemNotFoundException.cs
@@ -3,7 +3,7 @@
 namespace RFService.Exceptions
 {
     public class IdItemNotFoundException(Int64 id)
-        : HttpException(404, "Item with the ID \"{0}\" is not found.", id)
+        : HttpException(404, "Item with the ID \"{0}\" is not found.", id.ToString())
     {
     }
 }
