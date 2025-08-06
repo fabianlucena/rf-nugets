720a5504cb7e54668717512be34e77f65c2f3e2e
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2025-08-06 11:42:57 -0300

    RFDapper: fix IsValidObject for no ID.

--- RFDapper/Exceptions/CannotCheckObjectValitityBecauseTheObjectDoesNotHaveIdException.cs
+++
@@ -1,9 +0,0 @@
-﻿using RFHttpExceptions.Exceptions;
-using System.Text.Json;
-
-namespace RFDapper.Exceptions
-{
-    public class CannotCheckObjectValitityBecauseTheObjectDoesNotHaveIdException()
-        : HttpException(500, $"Cannot check object valitity because the object does not have ID.")
-    { }
-}
