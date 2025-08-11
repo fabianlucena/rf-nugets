c51cedceb55d4628e5d58fb943eee0f537a49aa2
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2025-08-11 18:58:14 -0300

    Create project RFRabbitMQClient.

---
+++ RFRabbitMQClient/Types/Request.cs
@@ -0,0 +1,12 @@
+﻿namespace RFRabbitMQClient.Types
+{
+    public class Request
+        : DataTransfer
+    {
+        public Request(byte[] data)
+            : base(data) { }
+
+        public Request(string data)
+            : base(data) { }
+    }
+}
