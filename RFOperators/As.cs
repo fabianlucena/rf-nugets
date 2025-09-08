950fb786bbca39efd8a2d00e8695ab90a26ca539
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2025-09-08 19:18:08 -0300

    RF Operators: add As, DataLength, and Sum operations.

---
+++ RFOperators/As.cs
@@ -0,0 +1,10 @@
+﻿namespace RFOperators
+{
+    public class As(Operator value, string alias)
+        : Unary(value)
+    {
+        public override int Precedence { get; } = 1;
+
+        public string Alias { get; } = alias;
+    }
+}
---
+++ RFOperators/DataLength.cs
@@ -0,0 +1,16 @@
+﻿namespace RFOperators
+{
+    public class DataLength
+        : Unary
+    {
+        public override int Precedence { get; } = 1;
+
+        public DataLength(Operator value)
+            : base(value)
+        { }
+
+        public DataLength(string column)
+            : base(column)
+        { }
+    }
+}
