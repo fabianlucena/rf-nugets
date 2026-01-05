a2aaecf800a30e032b84a0f32019da5008a2035c
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-01-05 18:43:29 -0300

    feat: add VirtualAttribute class for property attribute usage

--- RFService/Attributes/VirtualAttribute.cs
+++ RFService/Attributes/VirtualAttribute.cs
@@ -1,8 +1,7 @@
 ﻿namespace RFService.Attributes
 {
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
-    public class SizeAttribute(int size) : Attribute
+    public class VirtualAttribute() : Attribute
     {
-        public int Size { get; } = size;
     }
 }
