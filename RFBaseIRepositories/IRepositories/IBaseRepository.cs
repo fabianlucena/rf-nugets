e7b66025598a8fd93bf89a9feee095477efe47ce
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-04-30 21:55:31 -0300

    feat: add CreateDBSet method to IBaseRepository interface

--- RFBaseIRepositories/IRepositories/IBaseRepository.cs
+++ RFBaseIRepositories/IRepositories/IBaseRepository.cs
@@ -5,8 +5,8 @@ namespace RFBaseIRepositories.IRepositories
 {
     public interface IBaseRepository<T> where T : Base
     {
+        IQueryable<T> CreateDBSet(BaseQueryOptions? options = null);
         Task<T> CreateAsync(T entity);
-
         Task<IEnumerable<T>> GetListAsync(BaseQueryOptions? options = null);
     }
 }
