94039774e0aa3a3323e39f55c4595afb869f44d9
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-05-01 20:45:25 -0300

    feat: add TranslatableEntity repository and service implementations

---
+++ RFBaseIServices/IServices/ITranslatableEntityService.cs
@@ -0,0 +1,11 @@
+﻿using RFBaseEntities.Entities;
+using RFBaseEntities.QueryOptions;
+
+namespace RFBaseIServices.IServices
+{
+    public interface ITranslatableEntityService<T>
+        : ICommonEntityService<T>
+        where T : TranslatableEntity, new()
+    {
+    }
+}
---
+++ RFBaseServices/Services/TranslatableEntityService.cs
@@ -0,0 +1,19 @@
+﻿using RFBaseEntities.Entities;
+using RFBaseIRepositories.IRepositories;
+using RFBaseIServices.IServices;
+
+namespace RFBaseServices.Services
+{
+    public class TranslatableEntityService<T>(ITranslatableEntityRepository<T> repository)
+        : CommonEntityService<T>(repository),
+        ITranslatableEntityService<T>
+        where T : TranslatableEntity, new()
+    {
+        public override async Task<T> ValidateForCreateAsync(T entity)
+        {
+            entity = await base.ValidateForCreateAsync(entity);
+
+            return entity;
+        }
+    }
+}
