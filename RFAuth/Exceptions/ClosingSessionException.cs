1100e77253a281b3f37adef6611d19e99388b48d
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-05-15 14:55:38 -0300

    feat: remove ClosingSessionException class to streamline exception handling

--- RFAuth/Exceptions/ClosingSessionException.cs
+++
@@ -1,9 +0,0 @@
-﻿using RFHttpExceptions.Exceptions;
-
-namespace RFAuth.Exceptions
-{
-    public class ClosingSessionException()
-        : HttpException(500, "Closing session error")
-    {
-    }
-}
