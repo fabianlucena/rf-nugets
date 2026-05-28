30daac7e35387242063565d82bbce01473ebfb3c
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-05-28 14:44:41 -0300

    feat: add SeedDataAttribute class for method-level data seeding

---
+++ RFServices/Attributes/SeedDataAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace RFServices.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class SeedDataAttribute(bool isSystemData = false) : Attribute
+    {
+        public bool IsSystemData { get; } = isSystemData;
+    }
+}
+
