554f73a5e4f2a061c4a3ea1d36a81ca50e0c0d57
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-05-28 15:32:15 -0300

    feat: agregar espacio de nombres RFBase.Libs en LogService para mejorar la organización del código

--- RFLoggerProvider/Services/LogService.cs
+++ RFLoggerProvider/Services/LogService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using RFBase.Libs;
 using RFLogger.Types;
 using RFLoggerProvider.Entities;
 using RFLoggerProvider.Exceptions;
