899a3c27cbb13d45cd2d405bcec77dd07960cd61
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-05-29 15:05:52 -0300

    feat: refactor HttpExceptionL10nMiddleware and add RFServices project reference

--- RFHttpExceptionsL10n/Middlewares/HttpExceptionL10nMiddleware.cs
+++ RFHttpExceptionsL10n/Middlewares/HttpExceptionL10nMiddleware.cs
@@ -3,95 +3,97 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using RFBase.IExceptions;
 using RFL10n;
+using RFServices.Attributes;
 
-namespace RFHttpExceptionsL10n.Middlewares
+namespace RFHttpExceptionsL10n.Middlewares;
+
+
+[RegisterService]
+public class HttpExceptionL10nMiddleware(
+    ILogger<HttpExceptionL10nMiddleware> logger,
+    IServiceProvider provider
+)
+    : IMiddleware
 {
-    public class HttpExceptionL10nMiddleware(
-        ILogger<HttpExceptionL10nMiddleware> logger,
-        IServiceProvider provider
-    )
-        : IMiddleware
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
-        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        try
         {
-            try
-            {
-                await next(context);
-            }
-            catch (Exception exception)
-            {
-                await HandleExceptionAsync(context, exception);
-            }
+            await next(context);
         }
-
-        private async Task HandleExceptionAsync(
-            HttpContext context,
-            Exception exception
-        )
+        catch (Exception exception)
         {
-            string message;
-            string errorType;
+            await HandleExceptionAsync(context, exception);
+        }
+    }
+
+    private async Task HandleExceptionAsync(
+        HttpContext context,
+        Exception exception
+    )
+    {
+        string message;
+        string errorType;
+        try
+        {
+            if (exception is IHttpException httpException)
+            {
+                context.Response.StatusCode = httpException.StatusCode;
+
             try
             {
-                if (exception is IHttpException httpException)
+                using var scope = provider.CreateScope();
+                var l10n = scope.ServiceProvider.GetService<IL10n>();
+                if (l10n != null)
                 {
-                    context.Response.StatusCode = httpException.StatusCode;
-
-                try
-                {
-                    using var scope = provider.CreateScope();
-                    var l10n = scope.ServiceProvider.GetService<IL10n>();
-                    if (l10n != null)
-                    {
-                        message = await l10n._c(
-                            "exception",
-                            httpException.MessageFormat,
-                            httpException.Parameters
-                        );
-                    }
-                    else
-                    {
-                        message = httpException.Message;
-                    }
+                    message = await l10n._c(
+                        "exception",
+                        httpException.MessageFormat,
+                        httpException.Parameters
+                    );
                 }
-                catch (Exception e)
+                else
                 {
-                    logger.LogError(e, "Error translating exception message.");
-                    message = exception.Message;
+                    message = httpException.Message;
                 }
             }
-            else
+            catch (Exception e)
             {
-                context.Response.StatusCode = 500;
+                logger.LogError(e, "Error translating exception message.");
                 message = exception.Message;
             }
-
-                errorType = exception.GetType()
-                    ?.GetProperty("Error")
-                    ?.GetValue(exception)
-                    as string
-                    ?? exception.GetType().Name;
-            }
-            catch (Exception)
-            {
-                errorType = "UnknownError";
-                message = "An unexpected error occurred.";
-            }
-
-            var result = new
-            {
-                Error = errorType,
-                Message = message,
-            };
-
-            try
-            {
-                logger.LogError(exception, "{type}: {message}", errorType, message);
-            }
-            catch (Exception) { }
-
-            context.Response.ContentType = "application/json";
-            await context.Response.WriteAsJsonAsync(result);
         }
+        else
+        {
+            context.Response.StatusCode = 500;
+            message = exception.Message;
+        }
+
+            errorType = exception.GetType()
+                ?.GetProperty("Error")
+                ?.GetValue(exception)
+                as string
+                ?? exception.GetType().Name;
+        }
+        catch (Exception)
+        {
+            errorType = "UnknownError";
+            message = "An unexpected error occurred.";
+        }
+
+        var result = new
+        {
+            Error = errorType,
+            Message = message,
+        };
+
+        try
+        {
+            logger.LogError(exception, "{type}: {message}", errorType, message);
+        }
+        catch (Exception) { }
+
+        context.Response.ContentType = "application/json";
+        await context.Response.WriteAsJsonAsync(result);
     }
 }
