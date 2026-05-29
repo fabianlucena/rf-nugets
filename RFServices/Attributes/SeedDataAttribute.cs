cc8a77b4847e225d6aa4ba21488c992760cb5289
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-05-29 11:39:36 -0300

    feat: agregar RegisterServiceAttribute y refactorizar SeedDataAttribute para uso en clases; implementar AddAttributedServices en ServiceCollectionExtensions

--- RFServices/Attributes/SeedDataAttribute.cs
+++ RFServices/Attributes/SeedDataAttribute.cs
@@ -1,6 +1,6 @@
 ﻿namespace RFServices.Attributes
 {
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
     public class SeedDataAttribute(bool isSystemData = false) : Attribute
     {
         public bool IsSystemData { get; } = isSystemData;
--- RFServices/ServiceCollectionExtensions.cs
+++ RFServices/ServiceCollectionExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using RFIServices.IServices;
+using RFServices.Attributes;
 using RFServices.Services;
+using System.Reflection;
 
 namespace RFServices
 {
@@ -11,6 +13,64 @@ namespace RFServices
             services.AddScoped<IUserService, UserService>();
 
             return services;
+        }
+
+        public static IServiceCollection AddAttributedServices(this IServiceCollection services)
+        {
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(a => a.GetTypes())
+                .Where(t => t.GetCustomAttribute<RegisterServiceAttribute>() != null)
+                .Where(t => t.IsClass && !t.IsAbstract);
+
+            foreach (var type in types)
+            {
+                var attr = type.GetCustomAttribute<RegisterServiceAttribute>()!;
+                var interfaces = attr.Interfaces ?? [];
+
+                if (interfaces.Length == 0)
+                {
+                    var name = type.Name;
+                    var iName = "I" + name;
+                    var allInterfaces = type.GetInterfaces();
+
+                    if (allInterfaces.Length > 0)
+                    {
+                        interfaces = [.. allInterfaces.Where(i => i.Name == iName)];
+                        if (interfaces.Length == 0)
+                            interfaces = allInterfaces;
+                    }
+
+                    if (interfaces.Length == 0)
+                        interfaces = [type];
+                }
+
+                foreach (var iface in interfaces)
+                    services.Add(new ServiceDescriptor(iface, type, attr.Lifetime));
+            }
+
+            return services;
+        }
+
+        public static async Task ExecuteSeedersAsync(this IServiceProvider provider)
+        {
+            var seederTypes = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(a => a.GetTypes())
+                .Where(t => t.GetCustomAttribute<SeedDataAttribute>() != null)
+                .Where(t => t.IsClass && !t.IsAbstract)
+                .OrderBy(t => t.Name) // opcional: orden alfabético
+                .ToList();
+
+            foreach (var seederType in seederTypes)
+            {
+                var seeder = provider.GetRequiredService(seederType);
+
+                var runMethod = seederType.GetMethod("Run")
+                    ?? throw new InvalidOperationException($"{seederType.Name} no tiene un método Run().");
+                var result = runMethod.Invoke(seeder, null);
+
+                if (result is Task task)
+                    await task;
+            }
         }
     }
 }
