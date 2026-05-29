a9fb4d3ff4325d805556a2f15a78034a7f89df35
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-05-29 15:04:23 -0300

    feat: add RFServices reference in RFEntitiesEF

--- RFEntitiesEF/Repositories/EntityRepository.cs
+++ RFEntitiesEF/Repositories/EntityRepository.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using RFBase.ILibs;
 using RFEntities.Entities;
 using RFIServices.QueryOptions;
--- RFEntitiesEF/Repositories/UserRepository.cs
+++ RFEntitiesEF/Repositories/UserRepository.cs
@@ -2,86 +2,87 @@
 using RFEntities.Entities;
 using RFIRepositories.IRepositories;
 using RFIServices.QueryOptions;
+using RFServices.Attributes;
 
-namespace RFEntitiesEF.Repositories
+namespace RFEntitiesEF.Repositories;
+
+[RegisterService]
+public class UserRepository(DbContext context)
+    : CommonEntityRepository<User>(context),
+    IUserRepository
 {
-    public class UserRepository(DbContext context)
-        : CommonEntityRepository<User>(context),
-        IUserRepository
+    public override IQueryable<User> CreateDBSet(BaseQueryOptions? options = null)
     {
-        public override IQueryable<User> CreateDBSet(BaseQueryOptions? options = null)
+        var queryable = base.CreateDBSet(options);
+
+        if (options is UserQueryOptions userOptions)
         {
-            var queryable = base.CreateDBSet(options);
+            if (userOptions.Ids is not null)
+                queryable = queryable.Where(u => userOptions.Ids.Contains(u.Id));
 
-            if (options is UserQueryOptions userOptions)
-            {
-                if (userOptions.Ids is not null)
-                    queryable = queryable.Where(u => userOptions.Ids.Contains(u.Id));
+            if (userOptions.Username != null)
+                queryable = queryable.Where(u => u.Username == userOptions.Username);
 
-                if (userOptions.Username != null)
-                    queryable = queryable.Where(u => u.Username == userOptions.Username);
-
-                if (userOptions.TypeUuid != null)
-                    queryable = queryable
-                        .Include(u => u.Type)
-                        .Where(u => u.Type.Uuid == userOptions.TypeUuid);
-            }
-
-            return queryable;
+            if (userOptions.TypeUuid != null)
+                queryable = queryable
+                    .Include(u => u.Type)
+                    .Where(u => u.Type!.Uuid == userOptions.TypeUuid);
         }
 
-        public async Task<User> GetSingleByUsernameAsync(string username, UserQueryOptions? options = null)
+        return queryable;
+    }
+
+    public async Task<User> GetSingleByUsernameAsync(string username, UserQueryOptions? options = null)
+    {
+        return await GetSingleOrDefaultByUsernameAsync(username, options)
+            ?? throw new KeyNotFoundException($"User with username '{username}' not found.");
+    }
+
+    public async Task<long> GetSingleIdByUsernameAsync(string username, UserQueryOptions? options = null)
+    {
+        options ??= new UserQueryOptions();
+        options.Take = 2;
+        var set = GetDBSet(options);
+
+        var list = await set
+            .Where(u => u.Username == username)
+            .Select(u => u.Id)
+            .ToListAsync();
+
+        if (list.Count == 0)
+            throw new KeyNotFoundException($"User with username '{username}' not found.");
+
+        if (list.Count > 1)
+            throw new InvalidOperationException($"Multiple users with username '{username}' found.");
+        
+        return list[0];
+    }
+
+    public async Task<User?> GetSingleOrDefaultByUsernameAsync(string username, UserQueryOptions? options = null)
+    {
+        options ??= new UserQueryOptions();
+        options.Take = 2;
+        var set = GetDBSet(options);
+
+        var list = await set
+            .Where(u => u.Username == username)
+            .ToListAsync();
+
+        if (list.Count == 0)
+            return null;
+
+        if (list.Count > 1)
         {
-            return await GetSingleOrDefaultByUsernameAsync(username, options)
-                ?? throw new KeyNotFoundException($"User with username '{username}' not found.");
+            throw new InvalidOperationException($"Multiple users with username '{username}' found.");
         }
 
-        public async Task<long> GetSingleIdByUsernameAsync(string username, UserQueryOptions? options = null)
-        {
-            options ??= new UserQueryOptions();
-            options.Take = 2;
-            var set = GetDBSet(options);
+        return list[0];
+    }
 
-            var list = await set
-                .Where(u => u.Username == username)
-                .Select(u => u.Id)
-                .ToListAsync();
-
-            if (list.Count == 0)
-                throw new KeyNotFoundException($"User with username '{username}' not found.");
-
-            if (list.Count > 1)
-                throw new InvalidOperationException($"Multiple users with username '{username}' found.");
-            
-            return list[0];
-        }
-
-        public async Task<User?> GetSingleOrDefaultByUsernameAsync(string username, UserQueryOptions? options = null)
-        {
-            options ??= new UserQueryOptions();
-            options.Take = 2;
-            var set = GetDBSet(options);
-
-            var list = await set
-                .Where(u => u.Username == username)
-                .ToListAsync();
-
-            if (list.Count == 0)
-                return null;
-
-            if (list.Count > 1)
-            {
-                throw new InvalidOperationException($"Multiple users with username '{username}' found.");
-            }
-
-            return list[0];
-        }
-
-        public async Task<IEnumerable<string>> GetUsernamesAsync(UserQueryOptions options)
-        {
-            return await GetDBSet(options)
-                .Select(u => u.Username)
-                .ToListAsync();
-        }
+    public async Task<IEnumerable<string>> GetUsernamesAsync(UserQueryOptions options)
+    {
+        return await GetDBSet(options)
+            .Select(u => u.Username)
+            .ToListAsync();
     }
 }
