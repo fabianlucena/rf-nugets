90042f97743606f773d7e95b4dc65fe80e14dd2b
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-06-11 20:13:24 -0300

    feat: add UserEmailVerified configuration, repository, and service extension

---
+++ RFUserEmailVerifiedEF/Repositories/UserEmailVerifiedRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RFEntitiesEF.Repositories;
+using RFIServices.QueryOptions;
+using RFUserEmailVerified.Entities;
+using RFUserEmailVerified.IRepositories;
+using RFUserEmailVerified.QueryOptions;
+
+namespace RFUserEmailVerifiedEF.Repositories;
+
+public class UserEmailVerifiedRepository(DbContext context)
+    : CommonEntityRepository<UserEmailVerified>(context),
+    IUserEmailVerifiedRepository
+{
+    public override IQueryable<UserEmailVerified> CreateDBSet(BaseQueryOptions? options = null)
+    {
+        var queryable = base.CreateDBSet(options);
+
+        if (options is UserEmailVerifiedQueryOptions userEmailVerifiedQueryOptions)
+        {
+            if (userEmailVerifiedQueryOptions.UserId is not null)
+                queryable = queryable.Where(uev => uev.UserId == userEmailVerifiedQueryOptions.UserId);
+
+            if (userEmailVerifiedQueryOptions.Email is not null)
+                queryable = queryable.Where(uev => uev.Email == userEmailVerifiedQueryOptions.Email);
+        }
+
+        return queryable;
+    }
+}
---
+++ RFUserEmailVerifiedEF/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RFUserEmailVerified.IRepositories;
+using RFUserEmailVerifiedEF.Repositories;
+
+namespace RFUserEmailVerifiedEF;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddRFPermissionsEF(this IServiceCollection services)
+    {
+        services.AddScoped<IUserEmailVerifiedRepository, UserEmailVerifiedRepository>();
+
+        return services;
+    }
+}
