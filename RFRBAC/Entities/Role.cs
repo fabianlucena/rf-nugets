cecbdeefb2c3287b3c3135465056b4f6524ad8f3
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-06-26 11:53:53 -0300

    refactor: add Description property to Role entity

--- RFRBAC/Entities/Role.cs
+++ RFRBAC/Entities/Role.cs
@@ -12,6 +12,8 @@ namespace RFRBAC.Entities
         [Required]
         public bool IsSelectable { get; set; } = false;
 
+        public string Description { get; set; } = string.Empty;
+
         public Role() { }
 
         public Role(Role? entity = null)
