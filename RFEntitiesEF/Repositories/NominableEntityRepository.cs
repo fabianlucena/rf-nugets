047065d4760aef7f6a1406374e772a78c00def14
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-07-14 14:28:23 -0300

    refactor: clean up NominableEntityRepository class structure and improve readability

--- RFEntitiesEF/Repositories/NominableEntityRepository.cs
+++ RFEntitiesEF/Repositories/NominableEntityRepository.cs
@@ -2,35 +2,34 @@
 using RFEntities.Entities;
 using RFIServices.QueryOptions;
 
-namespace RFEntitiesEF.Repositories
+namespace RFEntitiesEF.Repositories;
+
+public class NominableEntityRepository<T>(DbContext context)
+    : CommonEntityRepository<T>(context)
+    where T : NominableEntity, new()
 {
-    public class NominableEntityRepository<T>(DbContext context)
-        : CommonEntityRepository<T>(context)
-        where T : NominableEntity, new()
+    public override IQueryable<T> CreateDBSet(BaseQueryOptions? options = null)
     {
-        public override IQueryable<T> CreateDBSet(BaseQueryOptions? options = null)
+        var queryable = base.CreateDBSet(options);
+
+        if (options is NominableEntityQueryOptions nominableQueryOptions)
         {
-            var queryable = base.CreateDBSet(options);
+            if (nominableQueryOptions.Name is not null)
+                queryable = queryable.Where(e => e.Name == nominableQueryOptions.Name);
 
-            if (options is NominableEntityQueryOptions nominableQueryOptions)
-            {
-                if (nominableQueryOptions.Name is not null)
-                    queryable = queryable.Where(e => e.Name == nominableQueryOptions.Name);
-
-                if (nominableQueryOptions.Names is not null)
-                    queryable = queryable.Where(e => nominableQueryOptions.Names.Contains(e.Name));
-            }
-
-            return queryable;
+            if (nominableQueryOptions.Names is not null)
+                queryable = queryable.Where(e => nominableQueryOptions.Names.Contains(e.Name));
         }
 
-        public async Task<IEnumerable<string>> GetNamesAsync(NominableEntityQueryOptions options)
-        {
-            var list = await GetDBSet(options)
-                .Select(e => e.Name)
-                .ToListAsync();
+        return queryable;
+    }
 
-            return list;
-        }
+    public async Task<IEnumerable<string>> GetNamesAsync(NominableEntityQueryOptions options)
+    {
+        var list = await GetDBSet(options)
+            .Select(e => e.Name)
+            .ToListAsync();
+
+        return list;
     }
 }
