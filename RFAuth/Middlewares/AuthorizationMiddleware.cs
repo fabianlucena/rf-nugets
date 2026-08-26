af734a1213bca3e2f23c50e9a9996e2109b6f881
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-08-25 21:01:53 -0300

    refactor: clean up AuthorizationMiddleware for improved readability and structure

--- RFAuth/Middlewares/AuthorizationMiddleware.cs
+++ RFAuth/Middlewares/AuthorizationMiddleware.cs
@@ -3,98 +3,97 @@ using RFAuth.QueryOptions;
 using RFAuth.IServices;
 using RFAuth.DTO;
 
-namespace RFAuth.Middlewares
+namespace RFAuth.Middlewares;
+
+public class AuthorizationMiddleware(RequestDelegate next)
 {
-    public class AuthorizationMiddleware(RequestDelegate next)
+    private static readonly Dictionary<string, CachedSession> cache = [];
+
+    public async Task InvokeAsync(
+        HttpContext context,
+        ISessionService sessionService
+    )
     {
-        private static readonly Dictionary<string, CachedSession> cache = [];
-
-        public async Task InvokeAsync(
-            HttpContext context,
-            ISessionService sessionService
-        )
+        if (context.Request.Headers.TryGetValue("Authorization", out var authorizationList)
+            && authorizationList.Count == 1)
         {
-            if (context.Request.Headers.TryGetValue("Authorization", out var authorizationList)
-                && authorizationList.Count == 1)
-            {
-                var cachedSession = await CheckAuthorizationAsync(
-                    authorizationList[0],
-                    sessionService
-                );
-                if (cachedSession is not null)
-                {
-                    foreach (var item in cachedSession.Items)
-                    {
-                        context.Items[item.Key] = item.Value;
-                    }
-
-                    await sessionService.UpdateLastUsageAsync(cachedSession.SessionId);
-                }
-            }
-
-            await next(context);
-        }
-
-        private static async Task<CachedSession?> CheckAuthorizationAsync(
-            string? authorization,
-            ISessionService sessionService
-        )
-        {
-            if (String.IsNullOrEmpty(authorization) || !authorization[..7].Equals("bearer ", StringComparison.CurrentCultureIgnoreCase))
-                return null;
-
-            var token = authorization[7..].Trim();
-            var cachedSession = await GetCachedSessionByTokenAsync(
-                token,
+            var cachedSession = await CheckAuthorizationAsync(
+                authorizationList[0],
                 sessionService
             );
-            if (cachedSession is null)
-                return cachedSession;
-
-            if (cachedSession.ExpireAt < DateTime.UtcNow)
+            if (cachedSession is not null)
             {
-                cache.Remove(token);
-                return null;
+                foreach (var item in cachedSession.Items)
+                {
+                    context.Items[item.Key] = item.Value;
+                }
+
+                await sessionService.UpdateLastUsageAsync(cachedSession.SessionId);
             }
-
-            await sessionService.UpdateLastUsageAsync(cachedSession.SessionId);
-
-            return cachedSession;
         }
 
-        private static async Task<CachedSession?> GetCachedSessionByTokenAsync(
-            string token,
-            ISessionService sessionService
-        )
+        await next(context);
+    }
+
+    private static async Task<CachedSession?> CheckAuthorizationAsync(
+        string? authorization,
+        ISessionService sessionService
+    )
+    {
+        if (String.IsNullOrEmpty(authorization) || !authorization[..7].Equals("bearer ", StringComparison.CurrentCultureIgnoreCase))
+            return null;
+
+        var token = authorization[7..].Trim();
+        var cachedSession = await GetCachedSessionByTokenAsync(
+            token,
+            sessionService
+        );
+        if (cachedSession is null)
+            return cachedSession;
+
+        if (cachedSession.ExpireAt < DateTime.UtcNow)
         {
-            if (cache.TryGetValue(token, out var cachedSession)
-                && cachedSession is not null)
-            {
-                return cachedSession;
-            }
+            cache.Remove(token);
+            return null;
+        }
 
-            var session = await sessionService.GetSingleOrDefaultByAuthorizationTokenAsync(token, new SessionQueryOptions { IncludeUser = true, IncludeDevice = true });
-            if (session is null || session.ExpireAt < DateTime.UtcNow || session.ClosedAt is not null)
-                return null;
+        await sessionService.UpdateLastUsageAsync(cachedSession.SessionId);
 
-            session = await sessionService.DecorateAsync(session);
+        return cachedSession;
+    }
 
-            cachedSession = new CachedSession(session);
-            cachedSession.Items["SessionId"] = session.Id;
-            cachedSession.Items["UserId"] = session.UserId;
-            cachedSession.Items["Session"] = session;
-            cachedSession.Items["User"] = session.User;
-            cachedSession.Items["Device"] = session.Device;
-            
-            foreach (var kv in session.ResponseData)
-                cachedSession.Items[kv.Key] = kv.Value;
-
-            foreach (var kv in session.InternalData)
-                cachedSession.Items[kv.Key] = kv.Value;
-
-            cache[token] = cachedSession;
-
+    private static async Task<CachedSession?> GetCachedSessionByTokenAsync(
+        string token,
+        ISessionService sessionService
+    )
+    {
+        if (cache.TryGetValue(token, out var cachedSession)
+            && cachedSession is not null)
+        {
             return cachedSession;
         }
+
+        var session = await sessionService.GetSingleOrDefaultByAuthorizationTokenAsync(token, new SessionQueryOptions { IncludeUser = true, IncludeDevice = true });
+        if (session is null || session.ExpireAt < DateTime.UtcNow || session.ClosedAt is not null)
+            return null;
+
+        session = await sessionService.DecorateAsync(session);
+
+        cachedSession = new CachedSession(session);
+        cachedSession.Items["SessionId"] = session.Id;
+        cachedSession.Items["UserId"] = session.UserId;
+        cachedSession.Items["Session"] = session;
+        cachedSession.Items["User"] = session.User;
+        cachedSession.Items["Device"] = session.Device;
+        
+        foreach (var kv in session.ResponseData)
+            cachedSession.Items[kv.Key] = kv.Value;
+
+        foreach (var kv in session.InternalData)
+            cachedSession.Items[kv.Key] = kv.Value;
+
+        cache[token] = cachedSession;
+
+        return cachedSession;
     }
 }
