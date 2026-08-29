142531cb06fe60417667fef350e35dd239e1ecca
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-08-29 20:33:21 -0300

    feat: add RestoreByIdAsync and RestoreByUuidAsync methods to ICommonEntityService

--- RFIServices/IServices/ICommonEntityService.cs
+++ RFIServices/IServices/ICommonEntityService.cs
@@ -1,10 +1,11 @@
 ﻿using RFEntities.Entities;
 
-namespace RFIServices.IServices
+namespace RFIServices.IServices;
+
+public interface ICommonEntityService<T>
+    : IAuditableEntityService<T>
+    where T : CommonEntity, new()
 {
-    public interface ICommonEntityService<T>
-        : IAuditableEntityService<T>
-        where T : CommonEntity, new()
-    {
-    }
+    Task<int> RestoreByIdAsync(long id);
+    Task<int> RestoreByUuidAsync(Guid uuid);
 }
