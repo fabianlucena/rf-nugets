9c86e6fc97e5ba9a288d9290cf407c6e2366dbea
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-08-30 13:55:45 -0300

    feat: add properties to OrganizationDTO

--- RFRGOBAC/DTO/OrganizationDTO.cs
+++ RFRGOBAC/DTO/OrganizationDTO.cs
@@ -5,6 +5,11 @@ namespace RFRGOBAC.DTO;
 public class OrganizationDTO(Organization Organization)
 {
     public Guid Uuid { get; } = Organization.Uuid;
+    public bool IsActive { get; } = Organization.IsActive;
     public string Name { get; } = Organization.Name;
     public string Title { get; } = Organization.Title;
+    public string Description { get; } = Organization.Description;
+    public DateTime CreatedAt { get; } = Organization.CreatedAt;
+    public DateTime UpdatedAt { get; } = Organization.UpdatedAt;
+    public DateTime? DeletedAt { get; } = Organization.DeletedAt;
 }
