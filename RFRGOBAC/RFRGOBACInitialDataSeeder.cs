4391f0c9647900be043c7f4d9c9c118680ebb891
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-08-29 21:35:49 -0300

    feat: add RFRGOBACInitialDataSeeder for seeding role permissions

---
+++ RFRGOBAC/RFRGOBACInitialDataSeeder.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RFAuth.IServices;
+using RFEntities.Entities;
+using RFIServices.IServices;
+using RFRolesPermissions.IServices;
+using RFServices.Attributes;
+using RFServices.Interfaces;
+
+namespace RFRGOBAC;
+
+[SeedData(true)]
+public class RFRGOBACInitialDataSeeder(
+    IServiceProvider serviceProvider
+) : ISeeder
+{
+    public async Task Run()
+    {
+        var addRolePermissionService = serviceProvider.GetService<IAddRolePermissionService>();
+        if (addRolePermissionService != null)
+        {
+            var rolesPermissions = new Dictionary<string, IEnumerable<string>>{
+                { "admin",  [
+                    "organizations.add", "organizations.get", "organizations.update", "organizations.delete", "organizations.restore",
+                ] },
+
+                { "user",  [
+                    "organizations.select",
+                ] },
+            };
+
+            await addRolePermissionService.AddRolesPermissionsAsync(rolesPermissions);
+        }
+    }
+}
