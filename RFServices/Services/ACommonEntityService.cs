15bd9cb8c5d06bce3030df23f6e32bad0d9ef73d
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-08-31 16:52:35 -0300

    feat: add ACommonEntityService for enhanced entity validation and management

---
+++ RFServices/Services/ACommonEntityService.cs
@@ -0,0 +1,21 @@
+﻿using RFEntities.Entities;
+using RFIRepositories.IRepositories;
+using RFIServices.IServices;
+
+namespace RFServices.Services;
+
+public class ACommonEntityService<T>(
+    IACommonEntityRepository<T> repository,
+    IServiceProvider serviceProvider
+)
+    : CommonEntityService<T>(repository, serviceProvider),
+    IACommonEntityService<T>
+    where T : ACommonEntity, new()
+{
+    public override async Task<T> ValidateForCreateAsync(T entity)
+    {
+        entity = await base.ValidateForCreateAsync(entity);
+
+        return entity;
+    }
+}
