fdf7ffa11e787766f900a8575889ffdeea59db44
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-09-01 19:44:16 -0300

    feat: define composite key for RoleXUserXOrganization entity

--- RFRGOBACEF/ETConfigurations/RoleXUserXOrganizationConfiguration.cs
+++ RFRGOBACEF/ETConfigurations/RoleXUserXOrganizationConfiguration.cs
@@ -12,21 +12,21 @@ public class RoleXUserXOrganizationConfiguration
     {
         base.Configure(entity);
 
-        entity.HasNoKey();
+        entity.HasKey(x => new { x.RoleId, x.UserId, x.OrganizationId });
 
         entity.HasOne(u => u.Role)
-              .WithMany()
-              .HasForeignKey(u => u.RoleId)
-              .OnDelete(DeleteBehavior.Restrict);
+            .WithMany()
+            .HasForeignKey(u => u.RoleId)
+            .OnDelete(DeleteBehavior.Restrict);
 
         entity.HasOne(u => u.User)
-              .WithMany()
-              .HasForeignKey(u => u.UserId)
-              .OnDelete(DeleteBehavior.Restrict);
+            .WithMany()
+            .HasForeignKey(u => u.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
 
         entity.HasOne(u => u.Organization)
-              .WithMany()
-              .HasForeignKey(u => u.OrganizationId)
-              .OnDelete(DeleteBehavior.Restrict);
+            .WithMany()
+            .HasForeignKey(u => u.OrganizationId)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
