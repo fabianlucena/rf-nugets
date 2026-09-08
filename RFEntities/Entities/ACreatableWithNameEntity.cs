84747c984e98c81dc201337338a6cb684825b9b6
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-09-08 19:12:42 -0300

    feat: add ACreatableWithNameEntity and ACreatableWithNameEntityQueryOptions classes for enhanced entity management

---
+++ RFEntities/Entities/ACreatableWithNameEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace RFEntities.Entities;
+
+public abstract class ACreatableWithNameEntity : CreatableWithNameEntity
+{
+    public bool IsActive { get; set; } = true;
+
+    public ACreatableWithNameEntity() { }
+
+    public ACreatableWithNameEntity(ACreatableWithNameEntity? entity = null)
+        : base(entity)
+    {
+        if (entity == null)
+            return;
+
+        IsActive = entity.IsActive;
+    }
+}
---
+++ RFIServices/QueryOptions/ACreatableWithNameEntityQueryOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace RFIServices.QueryOptions;
+
+public class ACreatableWithNameEntityQueryOptions : CreatableWithNameEntityQueryOptions
+{
+    public bool IncludeInactive { get; set; } = false;
+
+    public ACreatableWithNameEntityQueryOptions() { }
+
+    public ACreatableWithNameEntityQueryOptions(ACreatableWithNameEntityQueryOptions? options)
+        : base(options)
+    {
+        if (options == null)
+            return;
+
+        IncludeInactive = options.IncludeInactive;
+    }
+
+    public override ACreatableWithNameEntityQueryOptions Clone()
+        => new(this);
+}
