63d1d380e9e05f68b60975e98cde0bacd7865362
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-09-11 14:39:56 -0300

    feat: implement IGetCurrentAndSystemUserService interface for user retrieval methods

---
+++ RFIServices/IServices/IGetCurrentAndSystemUserService.cs
@@ -0,0 +1,13 @@
+﻿using RFEntities.Entities;
+
+namespace RFIServices.IServices;
+
+public interface IGetCurrentAndSystemUserService
+{
+    Task<User> GetSystemUserAsync();
+    Task<long> GetSystemUserIdAsync();
+    Task<User> GetCurrentUserAsync();
+    Task<long> GetCurrentUserIdAsync();
+    Task<User> GetCurrentOrSystemUserAsync();
+    Task<long> GetCurrentOrSystemUserIdAsync();
+}
--- RFIServices/IServices/IUserService.cs
+++ RFIServices/IServices/IUserService.cs
@@ -3,31 +3,14 @@ using RFIServices.QueryOptions;
 
 namespace RFIServices.IServices
 {
-    public interface IUserService : IACommonEntityService<User>
+    public interface IUserService : IACommonEntityService<User>, IGetCurrentAndSystemUser
     {
         Task<User> GetSingleByUsernameAsync(string username, UserQueryOptions? options = null);
         Task<User> GetOrCreateByUsernameAsync(string username, UserQueryOptions? options = null, Func<User, Task<User>>? createFactory = null);
-
         Task<long> GetSingleIdByUsernameAsync(string username, UserQueryOptions? options = null);
-
         Task<User?> GetSingleOrDefaultByUsernameAsync(string username, UserQueryOptions? options = null);
-
         Task<long?> GetSingleIdOrDefaultByUsernameAsync(string username, UserQueryOptions? options = null);
-
-        Task<User> GetSystemUserAsync();
-
-        Task<long> GetSystemUserIdAsync();
-
-        Task<User> GetCurrentUserAsync();
-
-        Task<long> GetCurrentUserIdAsync();
-
-        Task<User> GetCurrentOrSystemUserAsync();
-
-        Task<long> GetCurrentOrSystemUserIdAsync();
-
         Task UpdateLastLoginAtByUserIdAsync(long userId);
-
         Task<IEnumerable<string>> GetUsernamesByIdsAsync(IEnumerable<long> usersId, UserQueryOptions? options = null);
     }
 }
