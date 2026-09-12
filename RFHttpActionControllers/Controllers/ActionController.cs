7aaf672478b7790897b63d2061d5b877bf7c3239
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-09-12 01:40:57 -0300

    refactor: simplify ActionController structure and improve readability

--- RFHttpActionControllers/Controllers/ActionController.cs
+++ RFHttpActionControllers/Controllers/ActionController.cs
@@ -1,32 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using RFHttpAction.IServices;
 
-namespace RFHttpActionControllers.Controllers
-{
-    [ApiController]
-    [Route("v1/action")]
-    public class ActionController(
-        IHttpActionService httpActionService,
-        IHttpActionTypeService httpActionTypeService,
-        IHttpActionListeners httpActionListeners
-    ) : ControllerBase
-    {
-        [HttpPost("{token}")]
-        public async Task<IActionResult> PostAsync([FromRoute] string token)
-        {
-            var httpAction = await httpActionService.GetSingleByTokenAsync(token);
-            var httpActionType = await httpActionTypeService.GetSingleByIdAsync(httpAction.TypeId);
-            var listeners = httpActionListeners.GetListeners(httpActionType.Name);
-            if (listeners != null)
-            {
-                await httpActionService.DeleteByIdAsync(httpAction.Id);
-                foreach (var listener in listeners)
-                {
-                    await listener(httpAction);
-                }
-            }
+namespace RFHttpActionControllers.Controllers;
 
-            return Ok();
+[ApiController]
+[Route("v1/action")]
+public class ActionController(
+    IHttpActionService httpActionService,
+    IHttpActionTypeService httpActionTypeService,
+    IHttpActionListeners httpActionListeners
+) : ControllerBase
+{
+    [HttpPost("{token}")]
+    public async Task<IActionResult> PostAsync([FromRoute] string token)
+    {
+        var httpAction = await httpActionService.GetSingleByTokenAsync(token);
+        var httpActionType = await httpActionTypeService.GetSingleByIdAsync(httpAction.TypeId);
+        var listeners = httpActionListeners.GetListeners(httpActionType.Name);
+        if (listeners != null)
+        {
+            await httpActionService.DeleteByIdAsync(httpAction.Id);
+            foreach (var listener in listeners)
+            {
+                await listener(httpAction);
+            }
         }
+
+        return Ok();
     }
 }
