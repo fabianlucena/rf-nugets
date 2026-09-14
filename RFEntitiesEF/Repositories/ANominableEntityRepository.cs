0d7d38e402ebe4f425a1451a2cbc11515bb2850b
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-09-14 17:36:34 -0300

    feat: add ANominableEntityRepository to handle query options for active entities

---
+++ RFEntitiesEF/Repositories/ANominableEntityRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RFEntities.Entities;
+using RFIServices.QueryOptions;
+
+namespace RFEntitiesEF.Repositories;
+
+public class ANominableEntityRepository<T>(DbContext context)
+    : NominableEntityRepository<T>(context)
+    where T : ANominableEntity, new()
+{
+    public override IQueryable<T> CreateDBSet(BaseQueryOptions? options = null)
+    {
+        var queryable = base.CreateDBSet(options);
+
+        var includeInactive = false;
+
+        if (options is ANominableEntityQueryOptions aNominableOptions)
+        {
+            if (aNominableOptions.IncludeInactive)
+            {
+                includeInactive = true;
+            }
+        }
+
+        if (!includeInactive)
+        {
+            queryable = queryable.Where(u => u.IsActive);
+        }
+
+        return queryable;
+    }
+}
