208c564f3921ac84b4b0ab03b330f3310dd010b1
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-09-20 12:35:10 -0300

    fix: correct type for previousSessionId retrieval in ORGPDataService

--- RFRGOBAC/Services/ORGPDataService.cs
+++ RFRGOBAC/Services/ORGPDataService.cs
@@ -46,7 +46,7 @@ public class ORGPDataService(
 
             if (orpgData.CurrentOrganization is null)
             {
-                var previousSessionId = session.Data.GetLong("previousSessionId");
+                var previousSessionId = session.Data.GetInt64("previousSessionId");
                 orpgData.CurrentOrganization = await sessionOrganizationService.GetSingleOrDefaultOrganizationBySessionIdAsync(previousSessionId);
                 if (orpgData.CurrentOrganization is null)
                     return orpgData;
